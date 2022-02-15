c39c5821975811f503d365f2334507524b653727
Goldoni <fotsa.goldoni@yahoo.fr>
2022-02-16 00:21:02 +0100

    initial commit

--- Modules/Users/Resources/views/livewire/admin/users-datatable-component.blade.php
+++ Modules/Users/Resources/views/livewire/admin/users-datatable-component.blade.php
@@ -106,30 +106,39 @@
                     </x-slot>
                     <x-slot name="body">
                         @forelse ($rows as $row)
-                            <tr>
-                                <td class="px-6 py-4 whitespace-nowrap">
+                            <x-table.row>
+                                <x-table.cell>
                                     <div class="flex items-center">
                                         <div class="flex-shrink-0 h-10 w-10">
-                                            <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt="">
+                                            <img class="h-10 w-10 rounded-full" src="{{ $row->profile_photo_url }}" alt="">
                                         </div>
                                         <div class="ml-4">
                                             <div class="text-sm font-medium text-gray-900">{{ $row->name }}r</div>
                                             <div class="text-sm text-gray-500">{{ $row->email }}</div>
                                         </div>
                                     </div>
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap">
+                                </x-table.cell>
+                                <x-table.cell>
                                     <div class="text-sm text-gray-900">Regional Paradigm Technician</div>
                                     <div class="text-sm text-gray-500">Optimization</div>
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap">
+                                </x-table.cell>
+                                <x-table.cell>
                                     <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"> Active </span>
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">Admin</td>
-                                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                </x-table.cell>
+                                <x-table.cell class="text-sm text-gray-500">
+                                    @foreach ($row->roles as $role)
+                                        <a href="#" class="relative inline-flex items-center rounded-full border border-gray-300 px-3 py-0.5 m-1">
+                                            <div class="absolute flex-shrink-0 flex items-center justify-center">
+                                                <span class="h-1.5 w-1.5 rounded-full bg-indigo-500" aria-hidden="true"></span>
+                                            </div>
+                                            <div class="ml-3.5 text-sm font-medium text-gray-900">{{ $role->name }}</div>
+                                        </a>
+                                    @endforeach
+                                </x-table.cell>
+                                <x-table.cell class="text-right text-sm font-medium">
                                     <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                                </td>
-                            </tr>
+                                </x-table.cell>
+                            </x-table.row>
                         @empty
                         @endforelse
                     </x-slot>
