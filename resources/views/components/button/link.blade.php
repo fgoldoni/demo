390aaf242b8aea0746ab37c5f2c5011ba05bc138
Goldoni <fotsa.goldoni@yahoo.fr>
2022-02-20 15:21:23 +0100

    initial commit

--- resources/views/components/button/link.blade.php
+++ resources/views/components/button/link.blade.php
@@ -1,7 +1,7 @@
 <button
     {{ $attributes->merge([
         'type' => 'button',
-        'class' => 'text-' . config('settings.color') . '-700 text-sm leading-5 font-medium focus:outline-none focus:text-gray-800 focus:underline  hover:underline transition duration-150 ease-in-out' . ($attributes->get('disabled') ? ' opacity-75 cursor-not-allowed' : ''),
+        'class' => 'text-indigo-700 text-sm leading-5 font-medium focus:outline-none focus:text-gray-800 focus:underline  hover:underline transition duration-150 ease-in-out' . ($attributes->get('disabled') ? ' opacity-75 cursor-not-allowed' : ''),
     ]) }}
 >
     {{ $slot }}
--- resources/views/components/sidebar-link.blade.php
+++ resources/views/components/sidebar-link.blade.php
@@ -2,7 +2,7 @@
 
 @php
     $classes = ($active ?? false)
-                ? 'group bg-' . config('settings.color') . '-50 border-' . config('settings.color') . '-600 text-' . config('settings.color') . '-600 group border-l-4 py-2 px-3 flex items-center text-sm font-medium transition'
+                ? 'group bg-indigo-50 border-indigo-600 text-indigo-600 group border-l-4 py-2 px-3 flex items-center text-sm font-medium transition'
                 : 'group border-transparent text-gray-600 hover:text-gray-900 hover:bg-gray-50 group border-l-4 py-2 px-3 flex items-center text-sm font-medium transition';
 @endphp
 
